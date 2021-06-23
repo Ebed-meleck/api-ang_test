bd6338f5ddc34f17b39932e98a2857eb39a19419
Ebed meleck <ebedmeleckmakoso@gmail.com>
2021-06-23 14:54:17 +0100

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const authEndecode = (req, res, next) => {
   try {
     const secret = process.env.SESSION_HASH;
-    const token = req.headers.authorization.split(" ")[0];
+    const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, secret);
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
